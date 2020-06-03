4c31eadf428f21d878c97fd28cd9cb2de4918f0a
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-03 11:28:52 +0200

    ContentUsageMenuListener: Translate (simplification and clean-up)

--- src/bundle/EventListener/ContentUsageMenuListener.php
+++ src/bundle/EventListener/ContentUsageMenuListener.php
@@ -4,11 +4,10 @@ namespace AdrienDupuis\EzPlatformAdminBundle\EventListener;
 
 use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
 use EzSystems\EzPlatformAdminUi\Menu\MainMenuBuilder;
-use JMS\TranslationBundle\Translation\TranslationContainerInterface;
 use Knp\Menu\MenuItem;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class ContentUsageMenuListener implements EventSubscriberInterface //TranslationContainerInterface
+class ContentUsageMenuListener implements EventSubscriberInterface
 {
     const ITEM_CONTENT__CONTENT_USAGE = 'main__content__content_usage';
 
@@ -36,12 +35,5 @@ class ContentUsageMenuListener implements EventSubscriberInterface //Translation
                 ],
             ]
         );
-    }
-
-    public static function getTranslationMessages(): array
-    {
-        return [
-            (new Message('usage', 'ad_admin.content_usage'))->setDesc('Usage'),
-        ];
     }
 }
