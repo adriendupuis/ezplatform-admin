37089438fcb6b4ab88de88116114c93079a01682
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-03 10:52:04 +0200

    ContentUsageMenuListener: Translate

--- src/bundle/EventListener/ContentUsageMenuListener.php
+++ src/bundle/EventListener/ContentUsageMenuListener.php
@@ -4,27 +4,44 @@ namespace AdrienDupuis\EzPlatformAdminBundle\EventListener;
 
 use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
 use EzSystems\EzPlatformAdminUi\Menu\MainMenuBuilder;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+use Knp\Menu\MenuItem;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class ContentUsageMenuListener implements EventSubscriberInterface
+class ContentUsageMenuListener implements EventSubscriberInterface //TranslationContainerInterface
 {
-    public static function getSubscribedEvents()
+    const ITEM_CONTENT__CONTENT_USAGE = 'main__content__content_usage';
+
+    public static function getSubscribedEvents(): array
     {
         return [
             ConfigureMenuEvent::MAIN_MENU => ['onMenuConfigure', 0],
         ];
     }
 
-    public function onMenuConfigure(ConfigureMenuEvent $event)
+    public function onMenuConfigure(ConfigureMenuEvent $event): void
     {
+        /** @var MenuItem $menu */
         $menu = $event->getMenu();
+        /** @var MenuItem $menuItem */
+        $menuItem = $menu[MainMenuBuilder::ITEM_CONTENT];
 
-        $menu[MainMenuBuilder::ITEM_CONTENT]->addChild(
-            'content_stats',
+        $menuItem->addChild(
+            self::ITEM_CONTENT__CONTENT_USAGE,
             [
-                'label' => 'Usage',
+                'label' => 'usage',
                 'route' => 'ad_admin.content_usage',
+                'extras' => [
+                    'translation_domain' => 'ad_admin.content_usage',
+                ],
             ]
         );
+    }
+
+    public static function getTranslationMessages(): array
+    {
+        return [
+            (new Message('usage', 'ad_admin.content_usage'))->setDesc('Usage'),
+        ];
     }
 }
