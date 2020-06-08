edb7b186eed004e41fa4b69e283cdd9aa467c3df
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-08 17:35:18 +0200

    Move ExampleFinder dynamic config to a JS object

--- src/bundle/Resources/public/js/content_usage.js
+++ src/bundle/Resources/public/js/content_usage.js
@@ -1,8 +1,8 @@
 window.onload = function () {
     let exampleFinder = new ExampleFinder(
         '#example_finder_content_type',
-        tableBaseUrl,
-        searchBaseUrl,
+        exampleFinderConfig.layoutBaseUrl,
+        exampleFinderConfig.searchBaseUrl,
         '#example_finder_result',
         '#example_finder_status'
     );
