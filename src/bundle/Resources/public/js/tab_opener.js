8a90fc949589c0cc1bdb3eb1e27c306e8ed87589
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-09 11:52:21 +0200

    Open a tab from URL hash: Enable for main nav's tabs

--- src/bundle/Resources/public/js/tab_opener.js
+++ src/bundle/Resources/public/js/tab_opener.js
@@ -1,5 +1,10 @@
 $(function () {
-    if (document.location.hash.startsWith('#ez-tab')) {
-        $('[href="' + document.location.hash + '"]').click();
+    let tab = $('[href="' + document.location.hash + '"]');
+    if (tab.length) {
+        if (document.location.hash.startsWith('#ez-tab')) {
+            tab.click();
+        } else if ('tab' === tab.attr('role') && tab.parents('.ez-main-nav').length) {
+            document.location = $(document.location.hash).find('.nav-link').attr('href');
+        }
     }
 });
