b5c3bb84861ddea864ac8ca83bf0a8fbae650366
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-18 14:35:49 +0200

    Simplify reopen on reload: Re-use hash instead of introducing cookies

--- src/bundle/Resources/public/js/tab_opener.js
+++ src/bundle/Resources/public/js/tab_opener.js
@@ -1,8 +1,7 @@
 $(function () {
     let tab = $('[href="' + document.location.hash + '"]');
     if (tab.length) {
-        // Opening a tab in a new window uses an URL with a hash.
-        // If a tab is found for this hash, open it:
+        // Open tab designated by URL hash
         if (document.location.hash.startsWith('#ez-tab')) {
             tab.click();
         } else if ('tab' === tab.attr('role') && tab.parents('.ez-main-nav').length) {
@@ -26,32 +25,9 @@ $(function () {
             $('#tab-redirect-modal').modal('show');
             document.location = childTab.attr('href');
         }
-    } else {
-        // If reloading a page with a tab, reopen this tab:
-        var currentTabLocation = '', currentTabOwnHref = '';
-        for (let cookie of document.cookie.split(';')) {
-            let cookieCrumbs = cookie.trim().split('=', 2);
-            if (cookieCrumbs.length) {
-                if ('current_tab_location' === cookieCrumbs[0]) {
-                    currentTabLocation = cookieCrumbs[1];
-                } else if ('current_tab_own_href' === cookieCrumbs[0]) {
-                    currentTabOwnHref = cookieCrumbs[1];
-                }
-            }
-        }
-        console.log(document.cookie, currentTabLocation, currentTabOwnHref);
-        if (document.location.href === currentTabLocation) {
-            // Reopen the previously clicked tab
-            $('[href="' + currentTabOwnHref + '"]').click();
-        } else {
-            // Forgot the previously clicked tab
-            document.cookie = 'current_tab_location=; path=/; SameSite=Strict';
-            document.cookie = 'current_tab_own_href=; path=/; SameSite=Strict';
-        }
     }
-    $('[href^="#ez-tab"]').click(function () {
-        // Memorize the clicked tab
-        document.cookie = 'current_tab_location=' + document.location.href + '; path=/; SameSite=Strict';
-        document.cookie = 'current_tab_own_href=' + $(this).attr('href') + '; path=/; SameSite=Strict';
+    $('[href^="#ez-tab"]').click(function (event) {
+        // Memorize clicked tab
+        document.location.hash = $(this).attr('href');
     });
 });
