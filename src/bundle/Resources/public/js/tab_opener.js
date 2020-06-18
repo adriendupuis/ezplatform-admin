4bc8bdb387d57ccf28f0b8abfa7312b4258ecdf6
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-06-18 17:04:00 +0200

    tab_opener.js: Use template literal for modal's HTML
    https://developer.mozilla.org/docs/Web/JavaScript/Reference/Template_literals

--- src/bundle/Resources/public/js/tab_opener.js
+++ src/bundle/Resources/public/js/tab_opener.js
@@ -6,22 +6,24 @@ $(function () {
             tab.click();
         } else if ('tab' === tab.attr('role') && tab.parents('.ez-main-nav').length) {
             let childTab = $(document.location.hash).find('a.nav-link:first');
-            $('.ez-modal-wrapper').html(
-                '<div class="modal fade ez-modal" id="tab-redirect-modal" tabindex="-1" role="dialog">\n' +
-                '    <div class="modal-dialog modal-lg" role="document">\n' +
-                '        <div class="modal-content">\n' +
-                '            <div class="modal-header">' +
-                '               <h5 class="modal-title">' + Translator.trans(/*@Desc("Tab Redirect")*/ 'tab_opener.modal_title', {}, 'ad_admin_content_usage') + '</h5>' +
-                '            </div>' +
-                '            <div class="modal-body">' +
-                '                ' + Translator.trans(/*@Desc("Redirecting to tab")*/ 'tab_opener.modal_body', {}, 'ad_admin_content_usage') +
-                '                <em>' + tab.text().trim() + '</em>&nbsp;/' +
-                '                <strong><em>' + childTab.text().trim() + ' </em></strong>…' +
-                '            </div>' +
-                '        </div>' +
-                '    </div>' +
-                '</div>'
-            );
+            $('.ez-modal-wrapper').html(`
+                <div class="modal fade ez-modal" id="tab-redirect-modal" tabindex="-1" role="dialog">
+                    <div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">
+                                    ${Translator.trans(/*@Desc("Tab Redirect")*/ 'tab_opener.modal_title', {}, 'ad_admin_content_usage')}
+                                </h5>
+                            </div>
+                            <div class="modal-body">
+                                ${Translator.trans(/*@Desc("Redirecting to tab")*/ 'tab_opener.modal_body', {}, 'ad_admin_content_usage')}
+                                <em>${tab.text().trim()}</em>&nbsp;/
+                                <strong><em>${childTab.text().trim()}</em></strong>…
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `);
             $('#tab-redirect-modal').modal('show');
             document.location = childTab.attr('href');
         }
