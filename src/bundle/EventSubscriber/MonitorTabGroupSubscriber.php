e495286d2707e39305856ff8cc1d2d44c31c634e
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-08-04 16:07:43 +0200

    Monitor: Remove unneeded tab

---
+++ src/bundle/EventSubscriber/MonitorTabGroupSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AdrienDupuis\EzPlatformAdminBundle\EventSubscriber;
+
+use AdrienDupuis\EzPlatformAdminBundle\Tab\LandingPageUsage;
+use AdrienDupuis\EzPlatformAdminBundle\Tab\SearchEngineMonitor;
+use EzSystems\EzPlatformAdminUi\Tab\Event\TabEvents;
+use EzSystems\EzPlatformAdminUi\Tab\Event\TabGroupEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ContentUsageTabGroupSubscriber implements EventSubscriberInterface
+{
+    /** @var string */
+    public $searchEngine;
+
+    public function __construct(string $searchEngine)
+    {
+        $this->searchEngine = $searchEngine;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            TabEvents::TAB_GROUP_PRE_RENDER => ['onTabGroupPreRender', 10],
+        ];
+    }
+
+    public function onTabGroupPreRender(TabGroupEvent $event): void
+    {
+        $tabGroup = $event->getData();
+
+        if ('ad-admin-monitor' !== $tabGroup->getIdentifier()) {
+            return;
+        }
+
+        if (!in_array($this->searchEngine, SearchEngineMonitor::getSupportedSearchEngines())) {
+            $tabGroup->removeTab(SearchEngineMonitor::IDENTIFIER);
+        }
+    }
+}
