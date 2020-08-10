96fb8a67c20fb4f0c6e56794d8f44084d9f87bec
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-08-10 17:47:34 +0200

    Fix SearchEngineMonitorServiceAbstract::formatBytes thresholds

--- src/bundle/Service/SearchEngineMonitorServiceAbstract.php
+++ src/bundle/Service/SearchEngineMonitorServiceAbstract.php
@@ -12,27 +12,40 @@ abstract class SearchEngineMonitorServiceAbstract
      * Formats bytes using binary (default) base (1 KiB = 2^10 B = 1024 Bytes) base or metric base (1 kB = 10^3 B = 1000 B)
      * Returns a string ending with the largest unit possible and a starting with the corresponding float with $precision digits after the decimal separator.
      */
-    public static function formatBytes(float $bytes, int $precision = 2, string $unit = null, int $base = 1024): string
+    public static function formatBytes(int $bytes, int $precision = 2, string $unit = null, int $base = 1024): string
     {
-        $units = [
+        $unitSystems = [
             1000 => ['B', 'kB', 'MB', 'GB', 'TB'],
             1024 => ['B', 'KiB', 'MiB', 'GiB', 'TiB'],
         ];
-        if (array_key_exists($base, $units)) {
-            $baseUnits = $units[$base];
+        if (array_key_exists($base, $unitSystems)) {
+            $units = $unitSystems[$base];
         } else {
             throw new \InvalidArgumentException('Base must be '.implode(' or ', array_keys($units)));
         }
         if ($unit) {
-            $pow = array_search($unit, $baseUnits);
+            $pow = array_search($unit, $units);
             if (false === $pow) {
                 throw new \InvalidArgumentException("Unit '$unit' is invalid; available units for base $base: ".implode(', ', $baseUnits));
             }
         } else {
-            //$pow = max(0, min(floor(log($bytes, $base)), count($baseUnits) - 1));
-            $pow = max(0, min(round(log($bytes, $base)), count($baseUnits) - 1));
+            $pow = self::getUnitPower($bytes, $base, $units);
+            $formattedNumber = self::formatUnitNumber($bytes, $base, $pow, $precision);
+            $pow = self::getUnitPower(round(pow($base, $pow) * (float) $formattedNumber), $base, $units); // Avoid case like 1024.00 MiB
         }
 
-        return number_format($bytes / pow($base, $pow), $precision, '.', '').' '.$baseUnits[$pow];
+        if (0 === $pow) {
+            $precision = 0;
+        }
+        $formattedNumber = self::formatUnitNumber($bytes, $base, $pow, $precision);
+
+        return "$formattedNumber {$units[$pow]}";
+    }
+
+    private static function getUnitPower($bytes, $base, $units): int {
+        return max(0, min(floor(log($bytes, $base)), count($units) - 1));
+    }
+    private static function formatUnitNumber($bytes, $base, $pow, $precision): string {
+        return number_format($bytes / pow($base, $pow), $precision, '.', '');
     }
 }
--- tests/bundle/Service/SearchEngineMonitorServiceAbstractTest.php
+++ tests/bundle/Service/SearchEngineMonitorServiceAbstractTest.php
@@ -8,7 +8,7 @@ class SearchEngineMonitorServiceAbstractTest extends TestCase
     public function testBinaryFormatBytes()
     {
         $this->assertEquals(
-            '0.00 B',
+            '0 B',
             SearchEngineMonitorServiceAbstract::formatBytes(0)
         );
         $this->assertEquals(
@@ -17,7 +17,7 @@ class SearchEngineMonitorServiceAbstractTest extends TestCase
         );
 
         $this->assertEquals(
-            '1.00 B',
+            '1 B',
             SearchEngineMonitorServiceAbstract::formatBytes(1)
         );
         $this->assertEquals(
@@ -56,7 +56,7 @@ class SearchEngineMonitorServiceAbstractTest extends TestCase
     public function testMetricFormatBytes()
     {
         $this->assertEquals(
-            '0.00 B',
+            '0 B',
             SearchEngineMonitorServiceAbstract::formatBytes(0, 2, null, 1000)
         );
         $this->assertEquals(
@@ -65,7 +65,7 @@ class SearchEngineMonitorServiceAbstractTest extends TestCase
         );
 
         $this->assertEquals(
-            '1.00 B',
+            '1 B',
             SearchEngineMonitorServiceAbstract::formatBytes(1, 2, null, 1000)
         );
         $this->assertEquals(
@@ -103,69 +103,53 @@ class SearchEngineMonitorServiceAbstractTest extends TestCase
 
     public function testBinaryThresholds()
     {
-//        for ($b=1; $b<1280; $b++) {
-//            dump($b, SearchEngineMonitorServiceAbstract::formatBytes($b, 2, null, 1024));
-//        }
-
         $this->assertEquals(
-            '31.00 B',
-            SearchEngineMonitorServiceAbstract::formatBytes(31)
-        );
-        $this->assertEquals(
-            '0.03 KiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(32)
-        );
-        $this->assertEquals(
-            '0.99 KiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1018)
+            '1023 B',
+            SearchEngineMonitorServiceAbstract::formatBytes(1023)
         );
         $this->assertEquals(
             '1.00 KiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1019)
+            SearchEngineMonitorServiceAbstract::formatBytes(1024)
+        );
+        $this->assertEquals(
+            '1023.00 KiB',
+            SearchEngineMonitorServiceAbstract::formatBytes(1023*1024)
+        );
+        $this->assertEquals(
+            '1023.99 KiB',
+            SearchEngineMonitorServiceAbstract::formatBytes(1024*1024-6)
+        );
+        $this->assertEquals(
+            '1.00 MiB',
+            SearchEngineMonitorServiceAbstract::formatBytes(1024*1024-5)
+        );
+        $this->assertEquals(
+            '1023.00 MiB',
+            SearchEngineMonitorServiceAbstract::formatBytes(1023*1024*1024)
+        );
+        $this->assertEquals(
+            '1023.99 MiB',
+            SearchEngineMonitorServiceAbstract::formatBytes(1024*1024*1024-5*1024-123)
         );
         $this->assertEquals(
             '1.00 GiB'/*Avoid '1024.00 MiB'*/,
-            SearchEngineMonitorServiceAbstract::formatBytes(1023.995 * 1024 * 1024)
+            SearchEngineMonitorServiceAbstract::formatBytes(1024*1024*1024-5*1024-122)
         );
         $this->assertEquals(
-            '1024.00 MiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1023.995 * 1024 * 1024, 2, 'MiB')
-        );
-        $this->assertEquals(
-            '1.000 GiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1023.995 * 1024 * 1024, 3)
-        );
-        $this->assertEquals(
-            '1.0000 GiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1023.995 * 1024 * 1024, 4)
-        );
-        $this->assertEquals(
-            '1.00000 GiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1023.995 * 1024 * 1024, 5)
-        );
-        $this->assertEquals(
-            '0.999995 GiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1023.995 * 1024 * 1024, 6)
+            '1023.995 MiB',
+            SearchEngineMonitorServiceAbstract::formatBytes(1024*1024*1024-5*1024-122, 3)
         );
     }
 
     public function testMetricThresholds()
     {
         $this->assertEquals(
-            '31.00 B',
-            SearchEngineMonitorServiceAbstract::formatBytes(31, 2, null, 1000)
-        );
-        $this->assertEquals(
-            '0.03 kB',
-            SearchEngineMonitorServiceAbstract::formatBytes(32, 2, null, 1000)
-        );
-        $this->assertEquals(
-            '0.99 kB',
-            SearchEngineMonitorServiceAbstract::formatBytes(994, 2, null, 1000)
+            '999 B',
+            SearchEngineMonitorServiceAbstract::formatBytes(999, 2, null, 1000)
         );
         $this->assertEquals(
             '1.00 kB',
-            SearchEngineMonitorServiceAbstract::formatBytes(995, 2, null, 1000)
+            SearchEngineMonitorServiceAbstract::formatBytes(1000, 2, null, 1000)
         );
         $this->assertEquals(
             '1.00 MB'/*Avoid '1000.00 kB'*/,
@@ -176,20 +160,12 @@ class SearchEngineMonitorServiceAbstractTest extends TestCase
             SearchEngineMonitorServiceAbstract::formatBytes(999995, 2, 'kB', 1000)
         );
         $this->assertEquals(
-            '1.000 MB',
+            '999.995 kB',
             SearchEngineMonitorServiceAbstract::formatBytes(999995, 3, null, 1000)
         );
         $this->assertEquals(
-            '1.0000 MB',
+            '999.9950 kB',
             SearchEngineMonitorServiceAbstract::formatBytes(999995, 4, null, 1000)
-        );
-        $this->assertEquals(
-            '1.00000 MB',
-            SearchEngineMonitorServiceAbstract::formatBytes(999995, 5, null, 1000)
-        );
-        $this->assertEquals(
-            '0.999995 MB',
-            SearchEngineMonitorServiceAbstract::formatBytes(999995, 6, null, 1000)
         );
     }
 }
