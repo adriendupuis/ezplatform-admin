8b923662048b941c51e2287c6221846d8961a3d7
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-08-10 09:35:55 +0200

    MonitorService: clarify formatBytes used base: binary

--- src/bundle/Service/SearchEngineMonitorServiceAbstract.php
+++ src/bundle/Service/SearchEngineMonitorServiceAbstract.php
@@ -8,8 +8,13 @@ abstract class SearchEngineMonitorServiceAbstract
 
     abstract public function getOsMetrics(): array;
 
+    /**
+     * Formats bytes using binary base (1 KB = 2^10 B = 1024 Bytes) base.
+     * Returns a string ending with the largest unit possible and a starting with the corresponding float with $precision digits after the decimal separator.
+     */
     public static function formatBytes(float $bytes, int $precision = 2, string $unit = null): string
     {
+        $base = 1024;
         $units = ['B', 'KB', 'MB', 'GB', 'TB'];
         if ($unit) {
             $pow = array_search(strtoupper($unit), $units);
@@ -17,9 +22,9 @@ abstract class SearchEngineMonitorServiceAbstract
                 throw new \InvalidArgumentException("Unit '$unit' is invalid; available units: ".implode(', ', $units));
             }
         } else {
-            $pow = min(floor(log($bytes) / log(1024)), count($units) - 1);
+            $pow = min(floor(log($bytes, $base)), count($units) - 1);
         }
 
-        return round($bytes / pow(1024, $pow), $precision).' '.$units[$pow];
+        return round($bytes / pow($base, $pow), $precision).' '.$units[$pow];
     }
 }
