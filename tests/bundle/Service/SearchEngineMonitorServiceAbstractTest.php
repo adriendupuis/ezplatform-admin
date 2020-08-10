f360546c7b16323a92529ddf2da766c5c8fdd00a
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-08-10 10:37:01 +0200

    Format SearchEngineMonitorServiceAbstractTest.php

--- tests/bundle/Service/SearchEngineMonitorServiceAbstractTest.php
+++ tests/bundle/Service/SearchEngineMonitorServiceAbstractTest.php
@@ -9,23 +9,23 @@ class SearchEngineMonitorServiceAbstractTest extends TestCase
     {
         $this->assertEquals(
             '1.00 KiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(2**10)
+            SearchEngineMonitorServiceAbstract::formatBytes(2 ** 10)
         );
         $this->assertEquals(
             '1 KiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(2**10, 0)
+            SearchEngineMonitorServiceAbstract::formatBytes(2 ** 10, 0)
         );
         $this->assertEquals(
             '1.00 MiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(2**20)
+            SearchEngineMonitorServiceAbstract::formatBytes(2 ** 20)
         );
         $this->assertEquals(
             '1024.00 KiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(2**20, 2, 'KiB')
+            SearchEngineMonitorServiceAbstract::formatBytes(2 ** 20, 2, 'KiB')
         );
         $this->assertEquals(
             '1.50 GiB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1.5 * 2**30)
+            SearchEngineMonitorServiceAbstract::formatBytes(1.5 * 2 ** 30)
         );
     }
 
@@ -33,23 +33,23 @@ class SearchEngineMonitorServiceAbstractTest extends TestCase
     {
         $this->assertEquals(
             '1.00 kB',
-            SearchEngineMonitorServiceAbstract::formatBytes(10**3, 2, null, 1000)
+            SearchEngineMonitorServiceAbstract::formatBytes(10 ** 3, 2, null, 1000)
         );
         $this->assertEquals(
             '1 kB',
-            SearchEngineMonitorServiceAbstract::formatBytes(10**3, 0, null, 1000)
+            SearchEngineMonitorServiceAbstract::formatBytes(10 ** 3, 0, null, 1000)
         );
         $this->assertEquals(
             '1.00 MB',
-            SearchEngineMonitorServiceAbstract::formatBytes(10**6, 2, null, 1000)
+            SearchEngineMonitorServiceAbstract::formatBytes(10 ** 6, 2, null, 1000)
         );
         $this->assertEquals(
             '1000.00 kB',
-            SearchEngineMonitorServiceAbstract::formatBytes(10**6, 2, 'kB', 1000)
+            SearchEngineMonitorServiceAbstract::formatBytes(10 ** 6, 2, 'kB', 1000)
         );
         $this->assertEquals(
             '1.50 GB',
-            SearchEngineMonitorServiceAbstract::formatBytes(1.5 * 10**9, 2, null, 1000)
+            SearchEngineMonitorServiceAbstract::formatBytes(1.5 * 10 ** 9, 2, null, 1000)
         );
     }
 }
