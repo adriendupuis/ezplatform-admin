151aa3302eefa36a52f604bcd4e04aa6a8de151f
Adrien DUPUIS <adrien.dupuis@ez.no>
2021-04-01 11:53:57 +0200

    Clean MonitorController

--- src/bundle/Controller/MonitorController.php
+++ src/bundle/Controller/MonitorController.php
@@ -4,19 +4,9 @@ namespace AdrienDupuis\EzPlatformAdminBundle\Controller;
 
 use EzSystems\EzPlatformAdminUiBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MonitorController extends Controller
 {
-    /** @var TranslatorInterface */
-    private $translator;
-
-    public function __construct(
-        TranslatorInterface $translator
-    ) {
-        $this->translator = $translator;
-    }
-
     public function mainAction(): Response
     {
         return $this->render('@ezdesign/admin/monitor.html.twig');
